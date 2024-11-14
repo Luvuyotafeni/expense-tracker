71db86b1b3ccff11073af6c7239410968c56c309
Luvuyotafeni <luvuyotafeni012@gmail.com>
2024-11-14 05:27:51 -0800

    successfully shows the transactions

--- src/components/TranscationHistory.vue
+++ src/components/TranscationHistory.vue
@@ -1,10 +1,22 @@
 <template>
     <h3>Transaction History</h3>
     <ul id="list" class="list">
-        <li class="minus"> Cash <span> -R300</span> <button class="delete-btn">x</button></li>
-        <li class="plus"> Pay<span> R500</span> <button class="delete-btn">x</button></li>
-        <li class="plus"> Pay <span> R300</span> <button class="delete-btn">x</button></li>
-        <li class="minus"> Cash <span> -R500</span> <button class="delete-btn">x</button></li>
-        <li class="plus"> Pay<span> R200</span> <button class="delete-btn">x</button></li>
+        <li v-for="transaction in transactions " :key="transaction.id"> {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">x</button></li>
+        
     </ul>
 </template>
+<script>
+export default {
+    data() {
+        return{
+            transactions: [
+                { id: 1, text:'flour', amount: -999 },
+                { id: 2, text:'flour', amount: 999 },
+                { id: 3, text:'flour', amount: 999 },
+                { id: 4, text:'flour', amount: -699 },
+
+            ]
+        }
+    }
+}
+</script>
