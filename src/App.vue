97878bbd1ec515a8ee9dd11b11d9fb0ea3ec7523
Luvuyotafeni <luvuyotafeni012@gmail.com>
2024-11-14 04:37:31 -0800

    displayed the balance, expense and history

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
 <Header />
 <div class="container">
-  
+ <Balance/>
+ <BalanceExpenses/>
+ <TranscationHistory/>
 </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import BalanceExpenses from './components/BalanceExpenses.vue';
+import TranscationHistory from './components/TranscationHistory.vue';
+
 
 export default {
   components: {
     Header, 
+    Balance,
+    BalanceExpenses,
+    TranscationHistory
+
   }
 }
 </script>
---
+++ src/components/TranscationHistory.vue
@@ -0,0 +1,10 @@
+<template>
+    <h3>Transaction History</h3>
+    <ul id="list" class="list">
+        <li class="minus"> Cash <span> -R300</span> <button class="delete-btn">x</button></li>
+        <li class="plus"> Pay<span> R500</span> <button class="delete-btn">x</button></li>
+        <li class="plus"> Pay <span> R300</span> <button class="delete-btn">x</button></li>
+        <li class="minus"> Cash <span> -R500</span> <button class="delete-btn">x</button></li>
+        <li class="plus"> Pay<span> R200</span> <button class="delete-btn">x</button></li>
+    </ul>
+</template>
