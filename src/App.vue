570016aae066d93a5b67a9f033f0675ed8465dcf
Luvuyotafeni <luvuyotafeni012@gmail.com>
2024-11-14 05:46:21 -0800

    took the transactions to main page, then used define props to show on the transaction history

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,24 @@
 <div class="container">
  <Balance/>
  <BalanceExpenses/>
- <TranscationHistory/>
+ <TranscationHistory :transactions="transactions"/>
  <AddTransaction/>
 </div>
 </template>
 
-<script>
+<script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import BalanceExpenses from './components/BalanceExpenses.vue';
 import TranscationHistory from './components/TranscationHistory.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
+import { ref } from 'vue';
 
-export default {
-  components: {
-    Header, 
-    Balance,
-    BalanceExpenses,
-    TranscationHistory,
-    AddTransaction, 
-
-
-  }
-}
+const transactions = ref( [
+                { id: 1, text:'flour', amount: -999 },
+                { id: 2, text:'flour', amount: 999 },
+                { id: 3, text:'flour', amount: 999 },
+                { id: 4, text:'flour', amount: -699 },
+            ]);
 </script>
--- src/components/TranscationHistory.vue
+++ src/components/TranscationHistory.vue
@@ -1,22 +1,20 @@
 <template>
     <h3>Transaction History</h3>
     <ul id="list" class="list">
-        <li v-for="transaction in transactions " :key="transaction.id"> {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">x</button></li>
+        <li v-for="transaction in transactions" :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'"> {{ transaction.text }} <span>R{{ transaction.amount }}</span><button class="delete-btn">x</button></li>
         
     </ul>
 </template>
-<script>
-export default {
-    data() {
-        return{
-            transactions: [
-                { id: 1, text:'flour', amount: -999 },
-                { id: 2, text:'flour', amount: 999 },
-                { id: 3, text:'flour', amount: 999 },
-                { id: 4, text:'flour', amount: -699 },
 
-            ]
-        }
+<script setup>
+
+import { defineProps } from 'vue';
+
+const props = defineProps ({
+    transactions:{
+        type:Array,
+        required: true,
     }
-}
+})
 </script>
